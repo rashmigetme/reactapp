9cda7f8b8f7851ab18d5c758a4e0523e5fc9e42d
rashmigetme <rashmigetme@yahoo.co.in>
2023-01-15 22:11:30 -0500

    changed name and git origin for app.js file

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ function App() {
     <div className="App">
  <Container fluid>
           <h1 className="display-3">WELCOME TO BOOTCAMP - Jan 10 2023 Edition</h1>
-          <p className="lead">My Name is Sanjana Chandrashekar  </p>
-          <p className="lead"><a href="https://github.com/screact02">Here</a> is my Github Link  </p>
-          <p className="lead"><a href="http://webdevnotes.wynisco.com">Here</a> Here is Class Notes Link </p>
-          <p className="lead"><a href="http://webdevhw.wynisco.com">Here</a> Here is Homeworks Link </p>
+          <p className="lead">My Name is Rashmi Gupta  </p>
+          <p className="lead"><a href="https://github.com/rashmigetme">Here</a> is my Github Link  </p>
+          <p className="lead"><a href="http://webdevnotes.wynisco.com">Here</a> is Class Notes Link </p>
+          <p className="lead"><a href="http://webdevhw.wynisco.com">Here</a> is Homeworks Link </p>
           <p className="lead"><a href="https://docs.google.com/spreadsheets/d/1I75Kt78QSCRSAiF74yBttNyWVI4bq2ONsTWnN_ai9iA/edit?usp=sharing">Here</a> Main Google Spreadsheet </p>
 
      <DataTable />
